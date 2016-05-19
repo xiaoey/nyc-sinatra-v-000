2b227610b50dc44fa7cbe451750eed7cb4516512
Xiao Ling Zhu <xiaoeyzhu@gmail.com>
2016-05-19 18:50:39 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,19 @@
 require_relative 'application_controller'
 class FiguresController < ApplicationController
 
+get '/figures' do
+  @figures = Figure.all
+  erb :'/figures/index'
+end
+
+get '/figures/:id' do
+end
+
+post '/figures/:id' do
+end
+
+get '/figures/:id/edit' do
+  erb :'/figures/edit'
+end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,4 +1,9 @@
 require_relative 'application_controller'
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
 end
