cfd65304e6227acba2ef68f97a38772bdd975ac2
Xiao Ling Zhu <xiaoeyzhu@gmail.com>
2016-05-19 19:10:43 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,9 +1,9 @@
 require_relative 'application_controller'
 class LandmarksController < ApplicationController
 
-  get '/landmarks' do
-    @landmarks = Landmark.all
-    erb :'/landmarks/index'
-  end
+get '/landmarks' do
+  @landmarks = Landmark.all
+  erb :'/landmarks/index'
+end
 
 end
