1c10e084d4e5435dc3f1198680273f1d2e19e022
Xiao Ling Zhu <xiaoeyzhu@gmail.com>
2016-05-20 17:56:08 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,14 +6,36 @@ get '/figures' do
   erb :'/figures/index'
 end
 
-get '/figures/:id' do
+get '/figures/new' do
+  erb :'/figures/new'
 end
 
-post '/figures/:id' do
+post '/figures' do
+  @figure = Figure.create(:name => params[:figure][:name])
+  @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+  @figure.titles << Title.find_or_create_by(params[:title])
+  @figure.save
+  redirect "/figures/#{@figure.id}"
+end
+
+get '/figures/:id' do
+  @figure = Figure.find_by_id(params[:id])
+  erb :'/figures/show'
 end
 
 get '/figures/:id/edit' do
+  @landmark = Landmark.find_by_id(params[:id])
   erb :'/figures/edit'
+end
+
+post '/figures/:id' do
+  @figure = Figure.find_by_id(params[:id])
+  @figure.titles.clear
+  @figure.landmarks.clear
+  @figure.update(:name => params[:figure][:name])
+  create_or_update_landmark(params[:figure][:landmark_ids], params[:landmark][:name])
+  create_or_update_title(params[:figure][:title_ids], params[:title][:name])
+  redirect "/figures/#{@figure.id}"
 end
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,4 +6,29 @@ get '/landmarks' do
   erb :'/landmarks/index'
 end
 
+get '/landmarks/new' do
+  erb :'/landmarks/new'
+end
+
+post '/landmarks' do
+  @landmark = Landmark.create(:name => params[:landmark][:name], :year_completed => params[:landmark][:year_completed])
+  redirect "/landmarks/#{@landmark.id}"
+end
+
+get '/landmarks/:id' do
+  @landmark = Landmark.find_by_id(params[:id])
+  erb :'/landmarks/show'
+end
+
+get '/landmarks/:id/edit' do
+  @landmark = Landmark.find_by_id(params[:id])
+  erb :'/landmarks/edit'
+end
+
+post '/landmarks/:id' do
+  @landmark = Landmark.find_by_id(params[:id])
+  @landmark.update(:name => params[:landmark][:name], :year_completed => params[:landmark][:year_completed])
+  redirect "/landmarks/#{@landmark.id}"
+end
+
 end
